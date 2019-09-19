b87213ed9cc52c39cb36d9c12769baa1cfe3cbe6
jcharla <gotu0000@gmail.com>
2019-09-19 11:21:01 -0700

    1)File for generation of yearly based heatmap has been modified

--- HeatMapApproach/YearlyHM.py
+++ HeatMapApproach/YearlyHM.py
@@ -1,79 +1,58 @@
+import sys
+import os
 import numpy as np
-from AISDataManager import AISDataManager
-import Constants as c
 import pandas as pd
 
-import os
-
-#config parser
 import configparser
+sys.path.insert(0, '../Common/')
 
-#MyConfig.INI stores all the run time constants
+from AISDataManager import AISDataManager
+import Constants as c
+import HMUtils as hMUtil
+import TimeUtils as timeUtils
+
+runInCOEUS = 0
+
 config = configparser.ConfigParser()
-config.read('/home/jcharla/LiporLab/Conan/MyConfig.INI')
+
+if(runInCOEUS == 0):
+    config.read('../MyConfig.INI')
+else:
+    config.read('/home/jcharla/LiporLab/Conan/MyConfig.INI')
 
 from joblib import Parallel, delayed
 import multiprocessing
 
-#make object of AIS data manager
 aISDM = AISDataManager()
+numCores = multiprocessing.cpu_count()
+
+lonMin = (float)(config['REGION']['LON_MIN'])
+lonMax = (float)(config['REGION']['LON_MAX'])
+
+latMin = (float)(config['REGION']['LAT_MIN'])
+latMax = (float)(config['REGION']['LAT_MAX'])
+
+print("(lonMin , latMin) = (%f,%f)"%(lonMin,latMin))
+print("(lonMax , latMax) = (%f,%f)"%(lonMax,latMax))
 
 print("Starting Yearly Heat Map Generation")
 
-def generate_grid(xMin,xMax,yMin,yMax,step,precision):
-	xGrid = np.arange(xMin,xMax,step)
-	xGrid = np.around(xGrid,precision)
-	yGrid = np.arange(yMin,yMax,step)
-	yGrid = np.around(yGrid,precision)
+increStep = (float)(config['HEATMAP']['INCR_STEP'])
+incrRes = (int)(config['HEATMAP']['INCR_RES'])
 
-	xYMinMax = []
-	gridCounter = 0
-	for i in range(0,yGrid.shape[0]):
-	    for j in range(0,xGrid.shape[0]):
-	        if(i < (yGrid.shape[0]-1)):
-	            if(j < (xGrid.shape[0]-1)):
-	                localXMin = xGrid[j]
-	                localXMax = xGrid[j+1]
-	                localYMin = yGrid[i]
-	                localYMax = yGrid[i+1]
-	            else:
-	                localXMin = xGrid[j]
-	                localXMax = np.around(xGrid[j]+step,precision)
-	                localYMin = yGrid[i]
-	                localYMax = yGrid[i+1]
-	        else:
-	            if(j < (xGrid.shape[0]-1)):
-	                localXMin = xGrid[j]
-	                localXMax = xGrid[j+1]
-	                localYMin = yGrid[i]
-	                localYMax = np.around(yGrid[i]+step,precision)
-	            else:
-	                localXMin = xGrid[j]
-	                localXMax = np.around(xGrid[j]+step,precision)
-	                localYMin = yGrid[i]
-	                localYMax = np.around(yGrid[i]+step,precision)        
-	        xYMinMax.append([localXMin,localXMax,localYMin,localYMax,gridCounter])
-	        gridCounter = gridCounter + 1
-	return xGrid, yGrid, xYMinMax
-
-
-lonMin = (float)(config['REGEION']['LON_MIN'])
-lonMax = (float)(config['REGEION']['LON_MAX'])
-
-latMin = (float)(config['REGEION']['LAT_MIN'])
-latMax = (float)(config['REGEION']['LAT_MAX'])
-
-print(lonMin,latMin)
-print(lonMax,latMax)
-
-increStep = (float)(0.1)
-incrRes = (int)(1)
-
-heatMapGrid = generate_grid(lonMin, lonMax, latMin, latMax, increStep, incrRes)
-
+heatMapGrid = hMUtil.generate_grid(lonMin, lonMax, latMin, latMax, increStep, incrRes)
 boundaryArray = heatMapGrid[2]
 horizontalAxis = heatMapGrid[0]
 verticalAxis = heatMapGrid[1]
+
+# for boundary in boundaryArray:
+#     print(boundary)    
+# print(horizontalAxis)
+# print(horizontalAxis.shape)
+# print(verticalAxis)
+# print(verticalAxis.shape)
+
+opFile = "../Data/M120_00_M190_80_34_16_24_26/YearlyHM.npy"
 
 def compute_heat_map(localDf):
 	npHeatMap = np.zeros((horizontalAxis.shape[0]*verticalAxis.shape[0]))
@@ -85,29 +64,29 @@ def compute_heat_map(localDf):
 													)
 		vesselList = aISDM.get_list_of_unique_mmsi(boundedDF)
 		npHeatMap[i] = len(vesselList)
-		print("Done Computing")
-		print(i)
-	opFile = "/home/jcharla/LiporLab/Conan/YearlyHM.npy"
+		print("Done Computing %d"%(i))
 	np.save(opFile, npHeatMap)
 
-fileList = ["/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_01_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_02_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_03_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_04_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_05_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_06_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_07_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_08_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_09_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_10_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_11_LAP_DrSorted.csv" \
-			,"/home/jcharla/LiporLab/Data/AIS_0117_1217_31_M120_345_M117/AIS_2017_12_LAP_DrSorted.csv" \
-			]
+fileList = [ \
+            "../Data/M120_00_M190_80_34_16_24_26/17_01_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_02_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_03_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_04_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_05_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_06_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_07_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_08_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_09_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_10_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_11_Dropped.csv" \
+            ,"../Data/M120_00_M190_80_34_16_24_26/17_12_Dropped.csv" \
+            ]
 
 yearlyDF = pd.DataFrame()
 
 for file in fileList:
-	tempData,_ = aISDM.load_data_from_csv(file)
-	yearlyDF = yearlyDF.append(tempData, ignore_index = True)
+    tempData,_ = aISDM.load_data_from_csv(file)
+    yearlyDF = yearlyDF.append(tempData, ignore_index = True)
 
 compute_heat_map(yearlyDF)
+print("Done Generating Yearly Heat Map")
