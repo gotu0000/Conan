8271ca0cc8469209c9aed3852b16e2b5738395ce
jcharla <gotu0000@gmail.com>
2019-11-05 19:54:49 -0800

    1)Last entry data configurable from file

--- HeatMapApproach/GenLastEntryData.py
+++ HeatMapApproach/GenLastEntryData.py
@@ -32,71 +32,73 @@ numCores = 8
 print("Using %d cores"%(numCores))    
 
 #assumption is the directory contains souce files in numbered way
+SHIP_TYPE_DIR = (config['GEN_LAST_ENTRY']['SHIP_TYPE_DIR'])
+genCount = (int)(config['GEN_LAST_ENTRY']['GEN_COUNT'])
 
 sourceDir = [\
-            "../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr00/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr01/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr02/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr03/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr04/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr05/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr06/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr07/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr08/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr09/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr10/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr11/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr12/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr13/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr14/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr15/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr16/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr17/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr18/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr19/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr20/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr21/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr22/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr23/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr24/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr25/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr26/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr27/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr28/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr29/"\
+            SHIP_TYPE_DIR+"/HalfHr00/"\
+            ,SHIP_TYPE_DIR+"/HalfHr01/"\
+            ,SHIP_TYPE_DIR+"/HalfHr02/"\
+            ,SHIP_TYPE_DIR+"/HalfHr03/"\
+            ,SHIP_TYPE_DIR+"/HalfHr04/"\
+            ,SHIP_TYPE_DIR+"/HalfHr05/"\
+            ,SHIP_TYPE_DIR+"/HalfHr06/"\
+            ,SHIP_TYPE_DIR+"/HalfHr07/"\
+            ,SHIP_TYPE_DIR+"/HalfHr08/"\
+            ,SHIP_TYPE_DIR+"/HalfHr09/"\
+            ,SHIP_TYPE_DIR+"/HalfHr10/"\
+            ,SHIP_TYPE_DIR+"/HalfHr11/"\
+            ,SHIP_TYPE_DIR+"/HalfHr12/"\
+            ,SHIP_TYPE_DIR+"/HalfHr13/"\
+            ,SHIP_TYPE_DIR+"/HalfHr14/"\
+            ,SHIP_TYPE_DIR+"/HalfHr15/"\
+            ,SHIP_TYPE_DIR+"/HalfHr16/"\
+            ,SHIP_TYPE_DIR+"/HalfHr17/"\
+            ,SHIP_TYPE_DIR+"/HalfHr18/"\
+            ,SHIP_TYPE_DIR+"/HalfHr19/"\
+            ,SHIP_TYPE_DIR+"/HalfHr20/"\
+            ,SHIP_TYPE_DIR+"/HalfHr21/"\
+            ,SHIP_TYPE_DIR+"/HalfHr22/"\
+            ,SHIP_TYPE_DIR+"/HalfHr23/"\
+            ,SHIP_TYPE_DIR+"/HalfHr24/"\
+            ,SHIP_TYPE_DIR+"/HalfHr25/"\
+            ,SHIP_TYPE_DIR+"/HalfHr26/"\
+            ,SHIP_TYPE_DIR+"/HalfHr27/"\
+            ,SHIP_TYPE_DIR+"/HalfHr28/"\
+            ,SHIP_TYPE_DIR+"/HalfHr29/"\
             ]
 
 destDir = [\
-            "../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr00LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr01LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr02LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr03LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr04LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr05LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr06LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr07LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr08LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr09LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr10LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr11LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr12LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr13LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr14LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr15LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr16LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr17LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr18LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr19LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr20LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr21LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr22LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr23LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr24LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr25LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr26LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr27LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr28LE/"\
-            ,"../Data/M120_50_M119_00_33_90_34_44/Container/HalfHr29LE/"\
+            SHIP_TYPE_DIR+"/HalfHr00LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr01LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr02LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr03LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr04LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr05LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr06LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr07LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr08LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr09LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr10LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr11LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr12LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr13LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr14LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr15LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr16LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr17LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr18LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr19LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr20LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr21LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr22LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr23LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr24LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr25LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr26LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr27LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr28LE/"\
+            ,SHIP_TYPE_DIR+"/HalfHr29LE/"\
             ]
 
 def gen_last_entry_data(number,index):
@@ -110,153 +112,184 @@ def gen_last_entry_data(number,index):
     print(destFile)
     aISDM.save_data_to_csv(invertedSourceDFOnce,destFile)
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,0) \
-    for fileNumber in range(17519))
+if (genCount >= 0):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,0) \
+        for fileNumber in range(17519))
+    
+if(genCount >= 1):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,1) \
+        for fileNumber in range(17519))
+    
+if(genCount >= 2):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,2) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,1) \
-    for fileNumber in range(17519))
+if(genCount >= 3):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,3) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,2) \
-    for fileNumber in range(17519))
+if(genCount >= 4):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,4) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,3) \
-    for fileNumber in range(17519))
+if(genCount >= 5):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,5) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,4) \
-    for fileNumber in range(17519))
+if(genCount >= 6):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,6) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,5) \
-    for fileNumber in range(17519))
+if(genCount >= 7):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,7) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,6) \
-    for fileNumber in range(17519))
+if(genCount >= 8):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,8) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,7) \
-    for fileNumber in range(17519))
+if(genCount >= 9):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,9) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,8) \
-    for fileNumber in range(17519))
+if(genCount >= 10):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,10) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,9) \
-    for fileNumber in range(17519))
+if(genCount >= 11):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,11) \
+        for fileNumber in range(17519))    
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,10) \
-    for fileNumber in range(17519))
+if(genCount >= 12):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,12) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,11) \
-    for fileNumber in range(17519))    
+if(genCount >= 13):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,13) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,12) \
-    for fileNumber in range(17519))
+if(genCount >= 14):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,14) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,13) \
-    for fileNumber in range(17519))
+if(genCount >= 15):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,15) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,14) \
-    for fileNumber in range(17519))
+if(genCount >= 16):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,16) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,15) \
-    for fileNumber in range(17519))
+if(genCount >= 17):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,17) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,16) \
-    for fileNumber in range(17519))
+if(genCount >= 18):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,18) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,17) \
-    for fileNumber in range(17519))
+if(genCount >= 19):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,19) \
+        for fileNumber in range(17519))
+    
+if(genCount >= 20):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,20) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,18) \
-    for fileNumber in range(17519))
+if(genCount >= 21):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,21) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,19) \
-    for fileNumber in range(17519))
+if(genCount >= 22):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,22) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,20) \
-    for fileNumber in range(17519))
+if(genCount >= 23):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,23) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,21) \
-    for fileNumber in range(17519))
+if(genCount >= 24):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,24) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,22) \
-    for fileNumber in range(17519))
+if(genCount >= 25):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,25) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,23) \
-    for fileNumber in range(17519))
+if(genCount >= 26):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,26) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,24) \
-    for fileNumber in range(17519))
+if(genCount >= 27):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,27) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,25) \
-    for fileNumber in range(17519))
+if(genCount >= 28):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,28) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,26) \
-    for fileNumber in range(17519))
+if(genCount >= 29):
+    Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
+        (delayed(gen_last_entry_data) \
+        (fileNumber,29) \
+        for fileNumber in range(17519))
 
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,27) \
-    for fileNumber in range(17519))
-
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,28) \
-    for fileNumber in range(17519))
-
-Parallel(n_jobs=numCores, backend = 'multiprocessing', verbose=10) \
-    (delayed(gen_last_entry_data) \
-    (fileNumber,29) \
-    for fileNumber in range(17519))
 print("Done Generating The Data")
