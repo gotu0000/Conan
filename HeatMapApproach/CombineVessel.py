8c3c465b265a40263efb9bf1b33be71a3be12dd0
jcharla <gotu0000@gmail.com>
2020-02-17 11:17:49 -0800

    1)Segregation of vessel based on MMSI updated to take care of files with large memory 2)CombineVessel.py combines individual trajectories to make the complete trajectory for a year

---
+++ HeatMapApproach/CombineVessel.py
@@ -0,0 +1,89 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+
+import configparser
+sys.path.insert(0, '../Common/')
+
+from AISDataManager import AISDataManager
+import Constants as c
+import HMUtils as hMUtil
+import TimeUtils as timeUtils
+
+from joblib import Parallel, delayed
+import multiprocessing
+
+aISDM = AISDataManager()
+numCores = multiprocessing.cpu_count()
+
+#MyConfig.INI stores all the run time constants
+config = configparser.ConfigParser()
+config.read('../MyConfig.INI')
+
+SOURCE_DIR_NAME = (config['COMBINE_VESSEL']['SOURCE_DIR_NAME'])
+mMSIFile = (config['COMBINE_VESSEL']['MMSI_FILE'])
+destDir = (config['COMBINE_VESSEL']['DEST_DIR'])
+
+print(SOURCE_DIR_NAME)
+print(mMSIFile)
+print(destDir)
+#open list of MMSI file
+#and put it into list
+mMSIList = [line.rstrip('\n') for line in open(mMSIFile)]
+
+yearsToConsider = [ \
+                    # 16  \
+                    # ,17 \
+                    17 \
+                    ]
+
+
+monthToConsider = [ \
+                    timeUtils.month['Jan']    \
+                    ,timeUtils.month['Feb']   \
+                    ,timeUtils.month['March'] \
+                    ,timeUtils.month['April'] \
+                    ,timeUtils.month['May']   \
+                    ,timeUtils.month['June']  \
+                    ,timeUtils.month['July']  \
+                    ,timeUtils.month['Aug']   \
+                    ,timeUtils.month['Sept']  \
+                    ,timeUtils.month['Oct']   \
+                    ,timeUtils.month['Nov']   \
+                    ,timeUtils.month['Dec']   \
+                    ]
+
+destDirList = []
+
+for year in yearsToConsider:
+    for monthNum in monthToConsider:
+        destDirName = SOURCE_DIR_NAME+"MMSI_"+"%02d"%(year)+"/"+"%02d"%(monthNum)+"/"
+        destDirList.append(destDirName)
+
+for dirEn in destDirList:
+    print(dirEn)
+    
+
+def combine_vessel_data(name):
+    #empty dataframe initialization
+    yearTrajDF = pd.DataFrame()
+    for dirEn in destDirList:
+        monTrajFile = dirEn + name +'.csv'
+        print(monTrajFile)
+        #load and append monthly based data
+        monTrajDF,_ = aISDM.load_data_from_csv(monTrajFile)
+        yearTrajDF = yearTrajDF.append(monTrajDF, ignore_index = True)
+    #write it into file
+    yearTrajFile = destDir + name +'.csv'
+    aISDM.save_data_to_csv(yearTrajDF,yearTrajFile)
+    
+    #sort it with time
+    aISDM.formate_time(yearTrajDF,'DateTime',inPlace = True)
+    sortedYearTrajDF = yearTrajDF.sort_values(by='DateTime')
+    
+    yearTrajSortedFile = destDir + name +'_Sorted.csv'
+    aISDM.save_data_to_csv(sortedYearTrajDF,yearTrajSortedFile)
+
+for name in mMSIList:
+    combine_vessel_data(name)
--- HeatMapApproach/SegVessel.py
+++ HeatMapApproach/SegVessel.py
@@ -22,41 +22,54 @@ config = configparser.ConfigParser()
 config.read('../MyConfig.INI')
 
 SOURCE_DIR_NAME = (config['SEG_VESSEL']['SRC_DIR_NAME'])
-dirToStore = (config['SEG_VESSEL']['DEST_DIR'])
 mMSIFile = (config['SEG_VESSEL']['MMSI_FILE'])
 
+print(mMSIFile)
 #open list of MMSI file
 #and put it into list
 mMSIList = [line.rstrip('\n') for line in open(mMSIFile)]
 
-fileNameList = [ \
-            "../Data/"+SOURCE_DIR_NAME+"/16_01.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_02.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_03.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_04.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_05.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_06.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_07.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_08.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_09.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_10.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_11.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/16_12.csv" \
-            
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_01.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_02.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_03.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_04.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_05.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_06.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_07.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_08.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_09.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_10.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_11.csv" \
-            ,"../Data/"+SOURCE_DIR_NAME+"/17_12.csv" \
-            ]
+yearsToConsider = [ \
+                    # 16  \
+                    # ,17 \
+                    17 \
+                    ]
 
+
+monthToConsider = [ \
+                    timeUtils.month['Jan']    \
+                    ,timeUtils.month['Feb']   \
+                    ,timeUtils.month['March'] \
+                    ,timeUtils.month['April'] \
+                    ,timeUtils.month['May']   \
+                    ,timeUtils.month['June']  \
+                    ,timeUtils.month['July']  \
+                    ,timeUtils.month['Aug']   \
+                    ,timeUtils.month['Sept']  \
+                    ,timeUtils.month['Oct']   \
+                    ,timeUtils.month['Nov']   \
+                    ,timeUtils.month['Dec']   \
+                    ]
+
+fileNameList = []
+destDirList = []
+
+for year in yearsToConsider:
+    for monthNum in monthToConsider:
+        fileName = "../Data/"+SOURCE_DIR_NAME+"/"+"%02d"%(year)+"_"+"%02d"%(monthNum)+".csv"
+        fileNameList.append(fileName)
+        destDirName = "../Data/"+SOURCE_DIR_NAME+"/"+"MMSI_"+"%02d"%(year)+"/"+"%02d"%(monthNum)+"/"
+        destDirList.append(destDirName)
+
+
+for file in fileNameList:
+    print(file)
+
+for dir in destDirList:
+    print(dir)
+'''
+dirToStore = (config['SEG_VESSEL']['DEST_DIR'])
+print(dirToStore)
 data = pd.DataFrame()
 for file in fileNameList:
     tempData,_ = aISDM.load_data_from_csv(file)
@@ -82,3 +95,33 @@ def segregate_vessel_data(vesselName):
 # segregate_vessel_data(mMSIList[0])
 for name in mMSIList:
     segregate_vessel_data(name)
+'''
+
+
+def segregate_vessel_data(vesselName, dirToStore):
+    #filter based on MMSI
+    vesselData = aISDM.filter_based_on_mmsi(data, int(vesselName))
+
+    #get the name of file
+    fileName = dirToStore + vesselName + '.csv'
+    aISDM.save_data_to_csv(vesselData,fileName)
+
+    aISDM.formate_time(vesselData,'DateTime',inPlace = True)
+    sortedVD = vesselData.sort_values(by='DateTime')
+
+    fileName = dirToStore + vesselName + '_Sorted.csv'
+    print(fileName)
+    aISDM.save_data_to_csv(sortedVD,fileName)
+
+fileCounter = 0
+for file in fileNameList:
+
+    data,_ = aISDM.load_data_from_csv(file)
+
+    destDir = destDirList[fileCounter]
+
+    for name in mMSIList:
+        segregate_vessel_data(name,destDir)
+
+    fileCounter = fileCounter + 1
+
